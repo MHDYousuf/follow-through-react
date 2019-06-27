440843c698a1755c548d1cff90a182282a41e420
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2019-06-27 22:56:40 +0530

    creating first component

--- contactmanage/src/App.js
+++ contactmanage/src/App.js
@@ -1,30 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import Contact from "./components/Contact";
 function App() {
-  const name = "Yousuf";
-
-  const showHello = true;
-  const showMath = true;
-  let math;
-  if (showMath) {
-    math = (
-      <div>
-        <h4>1+1 = {1 + 1}</h4>
-        <h3>{numadd(10, 20)}</h3>
-      </div>
-    );
-  } else {
-    math = null;
-  }
-  function numadd(num1, num2) {
-    return num1 + num2;
-  }
   return (
     <div className="App">
       <h1>App Component</h1>
-      {math}
-      {showHello ? <h4>Hello {name.toUpperCase()} </h4> : null}
+      <Contact />
     </div>
   );
 }
---
+++ contactmanage/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h4>John Doe</h4>
+      <ul>
+        <li>Email : johndoe@gmail.com</li>
+        <li>Phone : 943566313</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contact;
