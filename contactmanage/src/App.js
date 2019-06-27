35180c28bbffb0740928c87d1f1bec45fb925ff5
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2019-06-27 22:50:25 +0530

    Familiarising with JSX Expressions

--- contactmanage/src/App.js
+++ contactmanage/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const name = "Yousuf";
+
+  const showHello = true;
+  const showMath = true;
+  let math;
+  if (showMath) {
+    math = (
+      <div>
+        <h4>1+1 = {1 + 1}</h4>
+        <h3>{numadd(10, 20)}</h3>
+      </div>
+    );
+  } else {
+    math = null;
+  }
+  function numadd(num1, num2) {
+    return num1 + num2;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App Component</h1>
+      {math}
+      {showHello ? <h4>Hello {name.toUpperCase()} </h4> : null}
     </div>
   );
 }
