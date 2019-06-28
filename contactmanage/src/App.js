b4e1e00f8fc2b4e796620024940f829a9e954b87
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2019-06-28 16:30:27 +0530

    Context API, Consumer, Proviers, Reducer

--- contactmanage/src/App.js
+++ contactmanage/src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./App.css";
 import Contacts from "./components/Contacts";
 import Header from "./components/Header";
+import { Provider } from "./context";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header Heading="Contact Manager" />
-      <div className="container">
-        <Contacts />
+    <Provider>
+      <div className="App">
+        <Header Heading="Contact Manager" />
+        <div className="container">
+          <Contacts />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
--- contactmanage/src/components/Contacts.js
+++ contactmanage/src/components/Contacts.js
@@ -1,49 +1,21 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "554-554-4564"
-      },
-      {
-        id: 2,
-        name: "Douglas",
-        email: "douglas@gmail.com",
-        phone: "554-5314-4564"
-      },
-      {
-        id: 3,
-        name: "Musammil",
-        email: "musa@gmail.com",
-        phone: "554-5134-4564"
-      }
-    ]
-  };
-
-  deleteContact = id => {
-    const { contacts } = this.state;
-    const newContacts = contacts.filter(contact => contact.id !== id);
-    this.setState({
-      contacts: newContacts
-    });
-  };
-
   render() {
-    const { contacts } = this.state;
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
