e2d34b60023e7ba251231b0bf1793a36dc109af0
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2019-06-30 18:21:36 +0530

    handling controlled component and onChange method

--- contactmanage/src/components/contacts/AddContact.js
+++ contactmanage/src/components/contacts/AddContact.js
@@ -6,13 +6,21 @@ class AddContact extends Component {
     email: "",
     phone: ""
   };
+  onChange = e =>
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
   render() {
     const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form action="">
+          <form action="" onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
@@ -21,6 +29,7 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter Name"
                 value={name}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
@@ -31,16 +40,18 @@ class AddContact extends Component {
                 className="form-control form-control-lg"
                 placeholder="Enter Email"
                 value={email}
+                onChange={this.onChange}
               />
             </div>
             <div className="form-group">
               <label htmlFor="phone">Phone</label>
               <input
-                type="text"
+                type="number"
                 name="phone"
                 className="form-control form-control-lg"
                 placeholder="Enter Phone"
                 value={phone}
+                onChange={this.onChange}
               />
             </div>
             <input
