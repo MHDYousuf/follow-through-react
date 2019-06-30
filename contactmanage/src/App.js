3b897b7f11f1afb7362a43a644ca9137b318645d
Mohammed Yousuf <36731361+myousuf07@users.noreply.github.com>
2019-06-30 18:11:05 +0530

    Interacting with forms

--- contactmanage/src/App.js
+++ contactmanage/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
-import Contacts from "./components/Contacts";
-import Header from "./components/Header";
+import Contacts from "./components/contacts/Contacts";
+import Header from "./components/layout/Header";
 import { Provider } from "./context";
+import AddContact from "./components/contacts/AddContact";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Header Heading="Contact Manager" />
         <div className="container">
+          <AddContact />
           <Contacts />
         </div>
       </div>
